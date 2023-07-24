1ee9a7ace3ea1220627efc16ae4ce9d02f3f5273
k7yusuf <K7yusuf@gmail.com>
2023-07-24 13:34:49 -0400

    I have Responsive Design and implemented couple assignments together. Also impleneted sass

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,120 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+$(document).ready(function() { 
+  // Function to escape text to prevent XSS
+const escape = function(str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
+const createTweetElement = function(tweet) {
+      const $tweet = $(`
+        <article class="tweet">
+          <header class="tweet-header">
+            <img class="avatar" src="${tweet.user.avatars}" alt="User Avatar">
+            <h4 class="tweet-author">${tweet.user.name}</h4>
+            <span class="tweet-handle">${tweet.user.handle}</span>
+          </header>
+          <div class="tweet-content">
+            <p class="tweet-text">${escape(tweet.content.text)}</p>
+          </div>
+          <footer class="tweet-footer">
+            <span class="tweet-timestamp">${timeago.format(tweet.created_at)}</span>
+            <div class="tweet-icons">
+              <i class="fas fa-flag"></i>
+              <i class="fas fa-retweet"></i>
+              <i class="fas fa-heart"></i>
+            </div>
+          </footer>
+        </article>
+      `);
+    
+      return $tweet;
+    };
 
+  // Function to toggle new-tweet section
+  const toggleNewTweet = function() {
+    const $newTweetSection = $('.new-tweet');
+
+    // Slide up the new-tweet section before toggling
+    $newTweetSection.slideToggle(function() {
+      if ($newTweetSection.is(':visible')) {
+        // Show textarea and focus on it
+        $('#tweet-text').focus();
+      } else {
+        // Hide textarea and reset form
+        $('#tweet-text').prop('disabled', true).val('');
+        $('.counter').text('140');
+        hideError();
+      }
+    });
+  };
+
+  // Attach click event to the "Write a new tweet" div
+  $('.write-tweet').click(toggleNewTweet);
+
+   //create tweet function ends
+  const renderTweets = function(tweets) {
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $('#tweets-container').prepend($tweet);
+    }
+  };
+
+  const loadTweets = function() {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+      dataType: 'json',
+      success: function(data) {
+        renderTweets(data);
+      },
+      error: function(err) {
+        console.log('Error fetching tweets:', err);
+      }
+    });
+  };
+
+  // Function to display error messages
+  const showError = function(message) {
+    const $errorMessage = $('.error-message');
+    $errorMessage.text(message);
+    $errorMessage.slideDown();
+  };
+
+  // Function to hide error messages
+  const hideError = function() {
+    $('.error-message').hide();
+  };
+
+  // Validate form before submitting
+  $('#tweet-form').submit(function(event) {
+    event.preventDefault();
+    hideError();
+
+    const tweetText = $('#tweet-text').val();
+
+    // Check if tweet is empty or exceeds the character limit
+    if (!tweetText || tweetText.trim() === '') {
+      showError('Error: Tweet content cannot be empty.');
+    } else if (tweetText.length > 140) {
+      showError('Error: Tweet content exceeds the character limit of 140.');
+    } else {
+      // Submit the form if validation passes
+      $.ajax({
+        url: '/tweets',
+        method: 'POST',
+        data: $(this).serialize(),
+        success: function() {
+          // Clear the textarea and reload tweets on successful submission
+          $('#tweet-text').val('');
+          loadTweets();
+        },
+        error: function(err) {
+          console.log('Error posting tweet:', err);
+        }
+      });
+    }
+  });
+
+  // Load tweets on page load
+  loadTweets();
+});
---
+++ public/scripts/composer.js
@@ -0,0 +1,162 @@
+$(document).ready(function() {
+  // Function to escape text to prevent XSS
+  const escape = function(str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
+  const createTweetElement = function(tweet) {
+    const $tweet = $(`
+      <article class="tweet">
+        <header class="tweet-header">
+          <img class="avatar" src="${tweet.user.avatars}" alt="User Avatar">
+          <h4 class="tweet-author">${tweet.user.name}</h4>
+          <span class="tweet-handle">${tweet.user.handle}</span>
+        </header>
+        <div class="tweet-content">
+          <p class="tweet-text">${escape(tweet.content.text)}</p>
+        </div>
+        <footer class="tweet-footer">
+          <span class="tweet-timestamp">${timeago.format(tweet.created_at)}</span>
+          <div class="tweet-icons">
+            <i class="fas fa-flag"></i>
+            <i class="fas fa-retweet"></i>
+            <i class="fas fa-heart"></i>
+          </div>
+        </footer>
+      </article>
+    `);
+
+    return $tweet;
+  };
+
+// // Function to toggle new-tweet section
+// const toggleNewTweet = function() {
+//   console.log("toggleNewTweet")
+//   const $newTweetSection = $('.new-tweet');
+
+//   // Show or hide the new-tweet section with a slide animation
+//   $newTweetSection.stop().slideToggle(function() {
+//     if ($newTweetSection.is(':visible')) {
+//       $newTweetSection.hide();
+//     } else {
+      
+//     }
+//   });
+// };
+
+// Keep track of the state of the new-tweet section
+let newTweetVisible = false;
+
+// Function to toggle new-tweet section
+const toggleNewTweet = function() {
+  console.log("toggleNewTweet")
+  const $newTweetSection = $('.new-tweet');
+
+  // If the section is currently visible, hide it and update the state
+  if (newTweetVisible) {
+    $newTweetSection.slideUp(function() {
+      newTweetVisible = false;
+    });
+  } else {
+    // If the section is currently hidden, show it and update the state
+    $newTweetSection.slideDown(function() {
+      newTweetVisible = true;
+      // Enable the textarea and focus on it when the section is shown
+      $('#tweet-text').prop('disabled', false).focus();
+    });
+  }
+};
+
+// Attach click event to the "Write a new tweet" div
+$('.write-tweet').click(toggleNewTweet);
+
+  // Function to display the scroll-to-top button
+  const $scrollButton = $('<div class="scroll-to-top-btn"><i class="fas fa-arrow-up"></i></div>');
+  $scrollButton.hide();
+  $('body').append($scrollButton);
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $scrollButton.fadeIn();
+      $('.write-tweet').fadeOut();
+    } else {
+      $scrollButton.fadeOut();
+      $('.write-tweet').fadeIn();
+    }
+  });
+
+  // Function to handle scroll-to-top button click
+  $scrollButton.click(function() {
+    $('html, body').animate({ scrollTop: 0 }, 800);
+    $('#tweet-text').prop('disabled', false).focus();
+  });
+
+  //create tweet function ends
+  const renderTweets = function(tweets) {
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $('#tweets-container').prepend($tweet);
+    }
+  };
+
+  const loadTweets = function() {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+      dataType: 'json',
+      success: function(data) {
+        renderTweets(data);
+      },
+      error: function(err) {
+        console.log('Error fetching tweets:', err);
+      }
+    });
+  };
+
+  // Function to display error messages
+  const showError = function(message) {
+    const $errorMessage = $('.error-message');
+    $errorMessage.text(message);
+    $errorMessage.slideDown();
+  };
+
+  // Function to hide error messages
+  const hideError = function() {
+    $('.error-message').hide();
+  };
+
+  // Validate form before submitting
+  $('#tweet-form').submit(function(event) {
+    event.preventDefault();
+    hideError();
+
+    const tweetText = $('#tweet-text').val();
+
+    // Check if tweet is empty or exceeds the character limit
+    if (!tweetText || tweetText.trim() === '') {
+      showError('Error: Tweet content cannot be empty.');
+    } else if (tweetText.length > 140) {
+      showError('Error: Tweet content exceeds the character limit of 140.');
+    } else {
+      // Submit the form if validation passes
+      $.ajax({
+        url: '/tweets',
+        method: 'POST',
+        data: $(this).serialize(),
+        success: function() {
+          // Clear the textarea and reload tweets on successful submission
+          $('#tweet-text').val('');
+          loadTweets();
+        },
+        error: function(err) {
+          console.log('Error posting tweet:', err);
+        }
+      });
+    }
+  });
+
+  // Load tweets on page load
+  loadTweets();
+});
